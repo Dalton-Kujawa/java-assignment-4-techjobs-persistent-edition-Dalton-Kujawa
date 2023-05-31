b9b5296bd8a86cae39b91318f047de840f650ae5
Dalton-Kujawa <daltonkujawa@yahoo.com>
2023-05-31 17:08:20 -0500

    finished with part two! on to part 3!

--- queries.sql
+++ queries.sql
@@ -1,6 +1,14 @@
 -- Part 1: Test it with SQL
 
+id INTEGER PRIMARY KEY AUTO INCREMENT
+employer VARCHAR(255)
+name VARCHAR(255)
+skill VARCHAR(255)
+
 -- Part 2: Test it with SQL
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -1,12 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotNull(message = "A name is required")
+    @Size(max = 100, message = "Names can not be longer than 100 characters")
     private String name;
 
     public int getId() {
